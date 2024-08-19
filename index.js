07b31f2d01281c656fea12dcff3ce99cc47904b7
rtdevcraft <hiiamabat@gmail.com>
2024-08-19 14:10:05 -0600

    deploying live

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 
 morgan.token("postData", (req) => {
   if (req.method === "POST") {
@@ -10,13 +11,13 @@ morgan.token("postData", (req) => {
   return "";
 });
 
-app.use(express.json());
-
 const customMorganFormat =
   ":method :url :status :res[content-length] :response-time ms :postData";
 
 app.use(express.json());
 app.use(morgan(customMorganFormat));
+app.use(cors());
+app.use(express.static("dist"));
 
 let persons = [
   {
@@ -41,10 +42,6 @@ let persons = [
   },
 ];
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
-
 let info = {
   content: `Phonebook has info for ${persons.length} people`,
   time: new Date().toString(),
@@ -61,6 +58,10 @@ app.get("/info", (request, response) => {
       <p>${info.content}</p>
       <p>${info.time}</p>
     `);
+});
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
@@ -122,6 +123,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
